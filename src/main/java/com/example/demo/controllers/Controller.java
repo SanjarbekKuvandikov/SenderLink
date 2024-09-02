ee8d8ef6884c036ab58c54ebfb82b7debcf7269e
sanjarbek <quvandiqovsanjarbek@gmail.com>
2024-09-02 12:49:07 +0500

    request added and it is work

--- src/main/java/com/example/demo/controllers/Controller.java
+++ src/main/java/com/example/demo/controllers/Controller.java
@@ -5,17 +5,14 @@ import com.example.demo.dtos.ResponseDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 public class Controller {
-    List<InformationDto> books = new ArrayList<>();
-
 
     @PostMapping(value = "/information")
     private ResponseEntity<ResponseDto> setInformation(@RequestBody InformationDto informationDto)
     {
+        System.out.println(informationDto.getFname()+" "+informationDto.getLname() + " " +informationDto.getTeloremail());
         return ResponseEntity.ok(
                 new ResponseDto("Successfully set information","success"));
     }
