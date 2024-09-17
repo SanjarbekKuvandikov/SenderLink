ffacaa4834b6b64e2c250efd4ecfd02a847444e4
sanjarbek <quvandiqovsanjarbek@gmail.com>
2024-09-17 13:43:06 +0500

    Data filtered , that user use its email or phone this will show in different(email and phone html) webpage

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,10 +1,21 @@
 var myApp = angular.module('app', []);
 
 myApp.controller("informationController", function ($scope, $http) {
-
+    $scope.emails = [];
+    $scope.phoneNumbers = [];
     $http.get('/information').then(function (response) {
         $scope.informations = response.data;
-        console.log("Success", response);
+        // Filter for emails (including id)
+        $scope.emails = $scope.informations.filter(function(info) {
+            return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(info.phoneOrEmail); // Email regex
+        });
+
+        // Filter for phone numbers (including id)
+        $scope.phoneNumbers = $scope.informations.filter(function(info) {
+            return /^\+?\d+$/.test(info.phoneOrEmail); // Adjusted phone number regex
+        });
+        console.log("Emails: ", $scope.emails);
+        console.log("Phone Numbers: ", $scope.phoneNumbers);
     }, function (error) {
         console.log("Error", error);
     });
@@ -20,15 +31,11 @@ myApp.controller("infoController", function ($scope, $http) {
             phoneOrEmail: phoneOrEmail
         };
         $scope.successMessage = "";
-
-        $scope.sendInformation = function(firstName, lastName, phoneOrEmail) {
             if (firstName && lastName && phoneOrEmail) {
                 $scope.successMessage = "Form sent successfully! We will get in touch with you shortly.";
             } else {
                 $scope.successMessage = "Please fill out all fields!";
             }
-        };
-
         $http.post('/information', {
             firstName: Information.firstName,
             lastName: Information.lastName,
